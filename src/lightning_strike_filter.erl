cee0d8a336ecb81d73df23d4eeb4170be09b6d6c
Anders Rassner <anders.rassner@gmail.com>
2020-06-04 14:58:44 +0200

    Initial code commit.
    Currently contains lots of commented code because of being a work in
    progress. Next thing to do is to add unit tests to prevent the
    LON_MIN being bigger than LON_MAX debacle from ever happening again.
    
    Currently lightning_warnings:check_for_strikes/0 reads a json file
    containing examples and filters out the strikes inside a preset
    lat/lon square. Things to do include adding the ability to pass a
    lat/lon square to the function, adding API calls to SMHI's
    Open Data API for it's Lightning Archive. Probably some way to only look
    at and/or request the strikes from the latest <TIME_PERIOD>. This might
    require changing this from a lib app to a normal otp app with supervisor
    structure and all that good jazz.

---
+++ src/lightning_strike_filter.erl
@@ -0,0 +1,36 @@
+%% Stuff
+-module(lightning_strike_filter).
+%% Mamlo Area
+%% -define(LAT_MAX, 55.65).
+%% -define(LAT_MIN, 55.48).
+%% -define(LON_MAX, 12.75).
+%% -define(LON_MIN, 13.23).
+%% Testing
+-define(LAT_MAX, 59.9209).
+-define(LAT_MIN, 59.6728).
+-define(LON_MAX, 17.2666).
+-define(LON_MIN, 16.5573).
+
+-export([strike_filter/1]).
+
+strike_filter(LightningStrikes) ->
+    strike_filter(LightningStrikes, []).
+
+strike_filter([], Acc) ->
+    {ok, lists:reverse(Acc)};
+strike_filter([LightningStrike | Rest], Acc) ->
+    #{<<"lat">> := Latitude, <<"lon">> := Longitude} = LightningStrike,
+    %%Latitude = maps:get(<<"lat">>, LightningStrike),
+    %%Longitude = maps:get(<<"lon">>, LightningStrike),
+    io:format("LAT_MAX ~p, LAT_MIN ~p, LON_MAX ~p, LON_MIN ~p\n", [?LAT_MAX, ?LAT_MIN, ?LON_MAX, ?LON_MIN]),
+    NewAcc = case {Latitude, Longitude} of
+        {Lat, Lon} when ((Lat >= ?LAT_MIN) and (Lat =< ?LAT_MAX) and (Lon >= ?LON_MIN) and (Lon =< ?LON_MAX)) ->
+            io:format("Strike with ~p,~p was kept\n", [Lat, Lon]),
+            lists:concat([Acc, [LightningStrike]]);
+        {Lat, Lon} -> 
+            io:format("Strike with ~p,~p was dropped\n", [Lat, Lon]),
+            Acc
+    end,
+    strike_filter(Rest, NewAcc).
+
+%% lightning_warnings:check_for_strikes().
